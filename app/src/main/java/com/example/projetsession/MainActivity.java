d3db41cf31b6d58aa921f3c65059fe84c265db58
Hans Bélanger <hans.belanger@pm.me>
2020-04-23 13:04:05 -0400

    api call for login working

--- app/src/main/java/com/example/projetsession/MainActivity.java
+++ app/src/main/java/com/example/projetsession/MainActivity.java
@@ -9,26 +9,52 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.Queue;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-
-        //idconfirmbutton
-
-        //idConfirm();
     }
 
     public void idConfirm(View v){
         Button idButton = (Button) v;
-        Log.i("DIM", "button click");
         TextView name = findViewById(R.id.idName);
-        Log.i("DIM",String.valueOf( name.getText()));
         TextView password = findViewById(R.id.password);
-        Log.i("DIM",String.valueOf( password.getText()));
 
+        final RequestQueue queue = Volley.newRequestQueue(this);
+
+
+        String url = "http://hansiv4.ddns.net:3000/user/"+name.getText()+"/"+password.getText();
+        Log.i("DIM",url);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("DIM", "Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+        queue.add(jsonObjectRequest);
 
     }
 
