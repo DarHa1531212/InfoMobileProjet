f061b56a26e53bea2205a8741747c5b03a648212
Simon <simon.helie-dabate@hotmail.com>
2020-04-23 12:04:20 -0400

    Login wip

--- app/src/main/java/com/example/projetsession/MainActivity.java
+++ app/src/main/java/com/example/projetsession/MainActivity.java
@@ -4,15 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.recipe_list);
+        setContentView(R.layout.login);
 
-        Intent intent = new Intent(MainActivity.this, feedActivity.class);
-        startActivity(intent);
+        //idconfirmbutton
+
+        //idConfirm();
     }
+
+    public void idConfirm(View v){
+        Button idButton = (Button) v;
+        Log.i("DIM", "button click");
+        TextView name = findViewById(R.id.idName);
+        Log.i("DIM",String.valueOf( name.getText()));
+        TextView password = findViewById(R.id.password);
+        Log.i("DIM",String.valueOf( password.getText()));
+
+
+    }
+
 }
