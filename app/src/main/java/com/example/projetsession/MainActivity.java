cd9e53bbbc1b3d5c1b9334bc61e8a3f51c1be9bd
Simon <simon.helie-dabate@hotmail.com>
2020-04-23 12:34:43 -0400

    Hans demands it!
    Code for the code gods.

--- app/src/main/java/com/example/projetsession/MainActivity.java
+++ app/src/main/java/com/example/projetsession/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.projetsession;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.projetsession.adapter.CustomRecipeAdapter;
+import com.example.projetsession.model.Recipe;
+
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,13 +42,35 @@ public class MainActivity extends AppCompatActivity {
         //idConfirm();
     }
 
+
     public void idConfirm(View v){
         Button idButton = (Button) v;
-        Log.i("DIM", "button click");
+
         TextView name = findViewById(R.id.idName);
-        Log.i("DIM",String.valueOf( name.getText()));
         TextView password = findViewById(R.id.password);
-        Log.i("DIM",String.valueOf( password.getText()));
+
+
+
+        final ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://hansiv4.ddns.net:3000/user/"+name.getText()+"/"+ password.getText();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("DIM", "Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
 
 
     }
